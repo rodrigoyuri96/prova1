2bc51bea625cd1854d36f8e365233a2812135d47
Rodrigo Yuri <rodrigoyuridejesuscordeiro1@gmail.com>
2021-04-23 10:28:35 -0300

    prova 1

--- README.md
+++ README.md
@@ -1,24 +1,20 @@
-# roudaum
+# PROVA 1
+Primeira prova para a turma de vuejs
 
-## Project setup
-```
-yarn install
-```
+## Instruções para execução do projeto:
+ * executar o comando **npm install**
+ * após a execução, observar se foi criada a pasta node-modules
+ * em seguida executar npm run serve para executar o projeto e acessar a porta que o terminal indicar. 
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
+CASO DE USO 1 (PRIMEIRA PARTE):
+  * O roudaum supermercados utiliza uma tela para cadastrar seus produtos. Um dos seus desenvolvedores pediu demissão sem aviso prévio e acabou abandonando a nova tela pela metade. Contudo, ele deixou uma lista de pendências para guiar os próximos desenvolvedores na implementação das funcionalidades.
+  
+  ## Tela de Cadastro:
+   * O variável produto foi criada, porém os campos do formulário não fazem binding com o objeto.
+   * O botão Salvar também foi criado, mas não existe comportamento. É necessário implementar um evento de click responsável por mostrar uma mensagem de sucesso para o usuário com o nome do respectivo produto.
+   * O botão Cancelar deve resetar os dados do forumlário 
 
-### Compiles and minifies for production
-```
-yarn build
-```
+CASO DE USO 1 (SEGUNDA PARTE):
+   * Na pré-visualização ("Verificação do two-way-data-bind"), quando clicar no elemento da tabela o produto selecionado (Macarrão) deverá ser carregado. 
+   * Dica: O esqueleto da função já foi desenhado, só falta a implementação. Nome da função:  "quandoSelecionaLinha()"  
 
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="roudaum-form">
-    <b-card class="ml-4">
-      <b-card-text>
-        <b-form @submit="quandoSubmeter" @reset="quandoResetar">
+    <b-card class="ml-4" header="Parte 1">
+      <b-card-text >
+        <b-card-title>Inclusão de um produto</b-card-title>
+        <b-form>
           <b-form-group
               id="input-group-1"
               label="Nome do produto"
@@ -27,34 +28,31 @@
           <b-button type="submit" variant="primary">Salvar</b-button>
           <b-button type="reset" variant="danger">Cancelar</b-button>
         </b-form>
-        <b-card class="mt-3" header="Verificação do two-way-data-binding">
-          <pre class="m-0">{{ produto }}</pre>
-        </b-card>
+
+
       </b-card-text>
-      <b-card-actions>
-        <b-table
-            :items="produtos"
-            :fields="colunas"
-            select-mode="single"
-            responsive="sm"
-            ref="selectableTable"
-            selectable
-            @row-selected="quandoSelecionaLinha"
-        >
-          <!-- Example scoped slot for select state illustrative purposes -->
-          <template #cell(selected)="{ rowSelected }">
-            <template v-if="rowSelected">
-              <span aria-hidden="true">&check;</span>
-              <span class="sr-only">Selecionado</span>
-            </template>
-            <template v-else>
-              <span aria-hidden="true">&nbsp;</span>
-              <span class="sr-only">Não selecionado</span>
-            </template>
-          </template>
-        </b-table>
-      </b-card-actions>
     </b-card>
+      <b-card class="mt-3 ml-4" header="Parte 2">
+        <b-card-title>Verificação do two-way-data-binding</b-card-title>
+
+        <pre class="m-0">{{ produtoSelecionado }}</pre>
+        <b-card-text>
+          <div>
+            <b-table
+                :items="produtos"
+                :fields="colunas"
+                select-mode="single"
+                responsive="sm"
+                ref="selectableTable"
+                selectable
+                @row-selected="quandoSelecionaLinha"
+            >
+            </b-table>
+          </div>
+        </b-card-text>
+      </b-card>
+
+
 
 
   </div>
@@ -77,25 +75,8 @@ export default {
     }
   },
   methods: {
-    quandoSubmeter(event) {
-      event.preventDefault()
-      alert(JSON.stringify(this.form))
-    },
-    quandoResetar(event) {
-      event.preventDefault()
-      // Reset our form values
-      this.form.email = ''
-      this.form.name = ''
-      this.form.food = null
-      this.form.checked = []
-      // Trick to reset/clear native browser form validation state
-      this.show = false
-      this.$nextTick(() => {
-        this.show = true
-      })
-    },
-    quandoSelecionaLinha(items) {
-      console.log(items)
+    quandoSelecionaLinha() {
+
     },
 
   }
